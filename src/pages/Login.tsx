561565eb11b96539b88047e7d59bd734271f384e
imrany <imranmat254@gmail.com>
2024-02-22 03:26:57 +0300

    updated login pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { useState } from "react"
-import { FaEye, FaInfoCircle } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaInfoCircle } from "react-icons/fa";
 import { auth, signInWithEmailAndPassword } from "../firebaseConfig/config";
+import { err_toast } from "../components/Feedback";
 
 function Login() {
     let [login_error,setLogin_error]=useState("")
@@ -11,12 +12,14 @@ function Login() {
         let password=document.getElementById("password");
         if(password?.getAttribute("type")=="password"){
           password?.setAttribute("type","text");
-          // setEye_icon(<EyeSlashIcon className="h-5 w-5"/>);
+          setEye_icon(<FaEyeSlash className="h-5 w-5"/>);
           return;
         }
         password?.setAttribute("type","password");
-        // setEye_icon(<EyeIcon className="h-5 w-5"/>);
+        setEye_icon(<FaEye className="h-5 w-5"/>);
     }
+
+    
 
     async function handleLogin(e:any){
         try {
@@ -32,6 +35,7 @@ function Login() {
             const errorCode = error.code;
             const errorMessage = error.message;
             console.log(error,errorCode,errorMessage)
+            err_toast(error.message)
         }
     }
     return (
@@ -39,20 +43,20 @@ function Login() {
             <div className="flex flex-col sm:w-[440px] max-sm:w-[85vw]">
                 <div className="sm:my-[40px] max-sm:my-[20px]">
                 <p className="text-[30px] text-[#1e293b] mb-[8px] font-semibold">Sign in</p>
-                <p className="text-[#64748b] text-[14px]">Enter your credentials to Login.</p>
+                <p className="text-[#64748b] text-[14px]">Enter your credentials to access Team Ruiri Portal.</p>
                     <p className="text-[var(--red-error)] text-xs mt-1 capitalize">{login_error}</p>
                 </div>
                 <form onSubmit={(e)=>handleLogin(e)} className="flex flex-col text-sm">
                 <div className="flex flex-col mb-3">
                     <label className="mb-[8px] font-semibold text-[#0f172a]" htmlFor="email">Email</label>
                     <div className="pb-4">
-                        <input id="email" name="email" type="email" className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`} placeholder="johndoe@gmail.com" required/>
+                        <input id="email" name="email" defaultValue={"johndoe@gmail.com"} type="email" className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`} placeholder="johndoe@gmail.com" required/>
                     </div>
 
                     <label htmlFor="password" className=" font-semibold mb-[8px] text-[#0f172a]">Password</label>
                     <div className="flex flex-col">
                         <div className="flex">
-                            <input id="password" name="password" type="password" className={`flex-grow px-[10px] py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-l-lg`} minLength={8} maxLength={24} required/>
+                            <input id="password" name="password" defaultValue={"12345678"} type="password" className={`flex-grow px-[10px] py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-l-lg`} minLength={8} maxLength={24} required/>
                             <button type="button" onClick={toggle_password} className="rounded-r-lg px-3 py-2 border-[1px] w-[53px] bg-white text-[#64748b]">
                             {eye_icon}
                             </button>
@@ -64,11 +68,11 @@ function Login() {
                 <button className="mt-5 capitalize py-3 px-6 text-white rounded-md bg-[var(--theme-blue)]">Sign in</button>
                 <div className="flex mt-5">
                     <p className="mr-3">{"Don't have an account"}</p>
-                    <Link to="/auth/sign_up" className="underline text-[var(--theme-blue)]">Create an Account</Link>
+                    <Link to="/register" className="underline text-[var(--theme-blue)]">Create an Account</Link>
                 </div>
                 <div className="mt-5 text-xs flex items-center gap-x-1 text-[var(--gray-text)]">
                     <FaInfoCircle className="w-5 h-5"/>
-                    <p>Only students and staff should sign in.</p>
+                    <p>Only members should sign in.</p>
                     </div>
                 </form>
             </div>
