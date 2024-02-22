d6ffa9d81cb6eb7537b69dd839d992c204e1cfcd
imrany <imranmat254@gmail.com>
2024-02-22 09:43:27 +0300

    added hero image

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,8 +1,9 @@
 function Notification() {
     return (
         <div className="flex flex-col">
-            <div className="w-full bg-[var(--theme-blue)] flex flex-col gap-10 items-center justify-center">
-                <p className="text-3xl font-semibold text-white">Team Ruiru</p>
+            <div id="notifcation_hero_image" className="w-full h-[60vh] text-[var(--theme-gray)] flex flex-col gap-2 items-center justify-center">
+                <p className="text-3xl font-semibold">Team Ruiru Notifications</p>
+                <p className="text-xl">Check out the latest posts from our team.</p>
             </div>
             <div className="p-10">
 
