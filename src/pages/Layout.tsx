d4a0846d16a8a5eaa7add95e871535c3d5fb7d8f
imrany <imranmat254@gmail.com>
2024-02-22 03:26:28 +0300

    updated nav bar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,23 +1,78 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import { auth, signOut } from "../firebaseConfig/config"
 
 export default function Layout(){
+  const location=useLocation()
+  async function logout(){
+    try{
+      await signOut(auth);
+      console.log("Successfull sign out")
+    }catch(error:any){
+      console.log(error)
+    }
+  }
+
+  let links=[
+    {
+      name:"Home",
+      to:"/"
+    },
+    {
+      name:"About",
+      to:"/about"
+    },
+    {
+      name:"Notification",
+      to:"/notification"
+    },
+    {
+      name:"Schedule",
+      to:"/schedule"
+    },
+    {
+      name:"Membership",
+      to:"/membership"
+    },
+    {
+      name:"Contributions",
+      to:"/contributions"
+    },
+    {
+      name:"Resources",
+      to:"/resources"
+    },
+    {
+      name:"Chat Room",
+      to:"/chat_room"
+    },
+    {
+      name:"Songs",
+      to:"/songs"
+    },
+    {
+      name:"Archives",
+      to:"/archives"
+    },
+    {
+      name:"Important",
+      to:"/important"
+    }
+  ]
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+      <nav className="mb-3 pt-3 border-b-[1px]">
+        <div className="px-2">
+         <p className="text-lg font-semibold">Team Ruiri Portal</p>
+	      </div>
+        <div className="flex text-white bg-[var(--theme-blue)] pr-2">
+	        {links.map((link,index)=>(<Link to={link.to} className={location.pathname===link.to?"px-2 py-3 bg-white text-[#213547]":"px-2 py-3 hover:bg-slate-200 hover:text-[#213547]"} key={index}>{link.name}</Link>))}
+          <button onClick={logout} className="px-2 hover:bg-slate-200 hover:text-[#213547]">Log out</button>
+        </div>
       </nav>
 
-      <Outlet />
+      <div className="px-1">
+        <Outlet />
+      </div>
     </>
   )
 };
