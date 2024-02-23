18a74e2dc8cc7d614c2b2365b4fc6a201b6be7e6
imrany <imranmat254@gmail.com>
2024-02-23 15:56:27 +0300

    done with resources page

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,7 +1,18 @@
 function Resources() {
     return (
-        <div>
-            
+        <div className="flex flex-col p-10">
+			<div>
+				<p className="text-3xl font-semibold">Resources</p>
+				<p>Some of the resources at our disposal include:</p> 
+				<ul style={{listStyle:'outside', margin:"0 0 0 20px"}}>
+                    <li>Bible lessons.</li>
+                    <li>Bible app- Swahili and English.</li>
+                    <li>Spritual Teachings and lessons, example feasts, baptism </li>
+                    <li>Motivation materials.</li>
+                    <li>Camp items.</li>
+                    <li>And other resources.</li>
+                </ul>
+			</div>
         </div>
     );
 };
