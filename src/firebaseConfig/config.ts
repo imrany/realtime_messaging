f4862c3384830acb2922730ae6ee454edbdebad9
imrany <imranmat254@gmail.com>
2024-02-23 09:50:22 +0300

    more firebase imports

--- src/firebaseConfig/config.ts
+++ src/firebaseConfig/config.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,9 +20,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db= getFirestore(app)
 
 export{
   auth,
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  onSnapshot,
+  doc,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
