4f15e6e732c882d8d89dfea7aee145ffecde51b2
imrany <imranmat254@gmail.com>
2024-02-23 10:58:59 +0300

    can now read,write and delete projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,11 @@
 import Select from "react-select"
 import { FaFileCsv, FaFilePdf, FaPlus } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { MdMoreVert } from "react-icons/md";
+import { MdClose, MdMoreVert } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { Project } from "../types/definitions";
+import { addDoc, collection, db, deleteDoc, doc, getDocs, onSnapshot } from "../firebaseConfig/config";
+import { err_toast } from "../components/Feedback";
 
 let sort_options=[
   {
@@ -15,44 +19,45 @@ let sort_options=[
 ]
 
 export default function Projects() {
-    let events=[
+    let [showAddProjectForm,setShowAddProjectForm]=useState(false)
+    let [projects,setProjects]=useState<Project[]>([
         {
-            date:"03/12/2023",
-            programme:"Community walk & Charity",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"03/01/2024",
-            programme:"Charity & Visiting orphanage",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"13/02/2024",
-            programme:"Sports & Marathon",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"15/02/2024",
-            programme:"Happiness & Fun fare",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"18/02/2024",
-            programme:"Charity & Village clean up activities",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"19/02/2024",
-            programme:"Charity & Visiting the sick",
-            location:"Ruiru, Nairobi"
+            project:"",
+            member_contributions:0,
+            account_for_payment:0
         }
-    ]
+    ])
+
+    const updateProject = onSnapshot(collection(db, "projects"), () => {
+        return "changed"
+    });
+
+    async function fetchProjectsFromFirebase(){
+        try {
+            const querySnapshot=await getDocs(collection(db,"projects"));
+            let list:Project[]=[]
+            querySnapshot.forEach((doc) => {
+                let data={
+                    id:doc.id,
+                    project:doc.data().project,
+                    member_contributions:doc.data().member_contributions,
+                    account_for_payment:doc.data().account_for_payment
+                }
+                list.push(data)
+            });
+            setProjects([...list])
+        } catch (error:any) {
+            console.log(error)
+            err_toast(error.message)
+        }
+    }
+
     function handleSort(value:string){
         console.log(value)
     }
 
     let checkedBoxArrayValues:string[]=[]
-    function checkedBoxHandler(e:any) {
+    async function checkedBoxHandler(e:any) {
         let header:any=document.getElementById("header")
         if(!e.target.checked){
             const index = checkedBoxArrayValues.indexOf(e.target.value);
@@ -90,10 +95,10 @@ export default function Projects() {
             checkedBoxArrayValues=[]
         })
 
-        document.getElementById("deleteCheckedItems")?.addEventListener("click",async()=>{
+        async function deleteEvent(){
             try{
-                checkedBoxArrayValues.map((checkedItem:string)=>{
-                  console.log(checkedItem)
+                checkedBoxArrayValues.map(async (checkedItem:string)=>{
+                    await deleteDoc(doc(db,"projects",checkedItem))
                 })
                 header.innerHTML=`<p class="text-lg font-semibold">Team Ruiru Portal</p>`
                 for (let i = 0; i < checkedBoxArrayValues.length; i++) {
@@ -103,16 +108,38 @@ export default function Projects() {
                 }
             }catch(error:any){
                 console.log(error.message)
+                err_toast(error.message)
             }
-        })
+        }
+        document.getElementById("deleteCheckedItems")?.addEventListener("click",deleteEvent)
     }
+
+    async function handleCreateProject(e:any) {
+        try {
+            e.preventDefault()
+            let event:Project={
+                project:e.target.project_name.value,
+                member_contributions:e.target.member_contributions.value,
+                account_for_payment:e.target.account_for_payment.value,
+            }
+            await addDoc(collection(db,"projects"),event);
+            e.target.reset()
+        } catch (error:any) {
+            console.log(error)
+            err_toast(error.message)
+        }
+    }
+
+    useEffect(()=>{
+        fetchProjectsFromFirebase()
+    },[updateProject])
     return (
-        <div className="p-10">
-            <div className="mt-8 w-full rounded-lg border-[1px] text-sm">
+        <div className="p-10 flex gap-4 max-sm:flex-wrap">
+            <div className="mt-8 flex-grow rounded-lg border-[1px] text-sm">
                 <div className="flex flex-col py-6 px-8 border-b-[1px]">
                     <div className="flex justify-between items-center">
                         <p className="text-[20px] text-[var(--gray-heading)] font-semibold">Projects</p>
-                        <button className="bg-[var(--theme-blue)] text-white flex rounded-md outline-none px-6 py-2 items-center justify-center">
+                        <button onClick={()=>setShowAddProjectForm(true)} className="bg-[var(--theme-blue)] text-white flex rounded-md outline-none px-6 py-2 items-center justify-center">
                             <FaPlus className="w-4 h-4 mr-1"/>
                             <span>Add a new project</span>
                         </button>
@@ -146,24 +173,24 @@ export default function Projects() {
                             <th className="text-left">
                                 <input type="checkbox" disabled className="w-5 border-gray-400 focus:bg-[var(--theme-blue)] accent-[var(--theme-blue)] cursor-pointer h-5"/>
                             </th> 
-                            <th className="text-left">Projects</th>
-                            <th className="text-left">Members Contributions</th>
-                            <th className="text-left">Account for Payment</th>
+                            <th className="text-left">Project</th>
+                            <th className="text-left">Member contributions</th>
+                            <th className="text-left">Account for payment</th>
                             <th className="text-left">Actions</th>
                         </tr>
                     </thead>
                     <tbody className='text-sm'>
-                        {events.map((schedule,index)=>{
+                        {projects.map((project)=>{
                             return(
-                                <tr title={`#${schedule.programme}`} key={index} className="text-[#64748B]">
+                                <tr title={`#${project.project}`} key={project.id} className="text-[#64748B]">
                                     <td className="text-left">
                                     <div>
-                                        <input id={`checkbox_${schedule.programme}`} type="checkbox" value={schedule.programme} onChange={checkedBoxHandler} className="checkbox w-5 border-gray-400 focus:bg-[var(--theme-blue)] accent-[var(--theme-blue)] cursor-pointer h-5"/>
+                                        <input id={`checkbox_${project.id}`} type="checkbox" value={project.id} onChange={checkedBoxHandler} className="checkbox w-5 border-gray-400 focus:bg-[var(--theme-blue)] accent-[var(--theme-blue)] cursor-pointer h-5"/>
                                     </div>
                                     </td>
-                                    <td className="text-left">{schedule.date}</td>
-                                    <td className="text-left">{schedule.programme}</td>
-                                    <td className="text-left">{schedule.location}</td>
+                                    <td className="text-left">{project.project}</td>
+                                    <td className="text-left">Ksh {project.member_contributions}</td>
+                                    <td className="text-left">{project.account_for_payment}</td>
                                     <td className="text-left" title="Actions">
                                         <div className="rounded-[100px] py-3 px-2 w-fit hover:shadow-md hover:bg-gray-100 cursor-pointer">
                                             <MdMoreVert className="w-6 h-4"/>
@@ -175,6 +202,31 @@ export default function Projects() {
                     </tbody>
                 </table>
             </div>
+            {showAddProjectForm?(
+                <div className="mt-8 h-fit rounded-lg border-[1px] text-sm p-4 w-[25vw]">
+                    <div className="flex justify-between items-center">
+                        <p className="text-[20px] font-semibold">Add a new project</p>
+                        <button title="close" onClick={()=>setShowAddProjectForm(false)}>
+                            <MdClose className="w-5 h-5"/>
+                        </button>
+                    </div>
+                    <form onSubmit={handleCreateProject} className="flex mt-5 flex-col text-sm">
+                        <label className="mb-[8px] text-[#0f172a]" htmlFor="project_name">Project Name <span className="text-red-500">*</span></label>
+                        <div className="pb-4">
+                            <input id="project_name" name="project_name" type="text" className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`} placeholder="Visiting the Sick" required/>
+                        </div>
+                        <label className="mb-[8px] text-[#0f172a]" htmlFor="member_contributions"> Member Contributions <i className="text-gray-500 text-sm ml-2">Ksh</i></label>
+                        <div className="pb-4">
+                            <input id="member_contributions" name="member_contributions" type="number" className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`}/>
+                        </div>
+                        <label className="mb-[8px] text-[#0f172a]" htmlFor="account_for_payment"> Account for payment Number <span className="text-red-500">*</span></label>
+                        <div className="pb-4">
+                            <input id="account_for_payment" name="account_for_payment" type="tel" className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`} placeholder="254734XXXXXX" required/>
+                        </div>
+                        <button className="mt-5 capitalize py-3 px-6 text-white rounded-md bg-[var(--theme-blue)]">Submit</button>
+                    </form>
+                </div>
+            ):""}
         </div>
     );
 };
