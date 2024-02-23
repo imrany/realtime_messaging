d2f296ff3cedf7c0ecdd222dcc5c95f5abd4c1ef
imrany <imranmat254@gmail.com>
2024-02-23 11:38:17 +0300

    can now read, write and delete songs

--- src/pages/Songs.tsx
+++ src/pages/Songs.tsx
@@ -1,7 +1,11 @@
 import Select from "react-select"
 import { FaFileCsv, FaFilePdf, FaPlus } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { MdMoreVert } from "react-icons/md";
+import { MdClose, MdMoreVert } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { Song } from "../types/definitions";
+import { addDoc, collection, db, deleteDoc, doc, getDocs, onSnapshot } from "../firebaseConfig/config";
+import { err_toast } from "../components/Feedback";
 
 let sort_options=[
   {
@@ -15,44 +19,43 @@ let sort_options=[
 ]
 
 export default function Songs() {
-    let events=[
+    let [showAddSongForm,setShowAddSongForm]=useState(false)
+    let [songs,setSongs]=useState<Song[]>([
         {
-            date:"03/12/2023",
-            programme:"Community walk & Charity",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"03/01/2024",
-            programme:"Charity & Visiting orphanage",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"13/02/2024",
-            programme:"Sports & Marathon",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"15/02/2024",
-            programme:"Happiness & Fun fare",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"18/02/2024",
-            programme:"Charity & Village clean up activities",
-            location:"Ruiru, Nairobi"
-        },
-        {
-            date:"19/02/2024",
-            programme:"Charity & Visiting the sick",
-            location:"Ruiru, Nairobi"
+            song_name:"",
+            youtube_link:"",
         }
-    ]
+    ])
+
+    const updateSong = onSnapshot(collection(db, "songs"), () => {
+        return "changed"
+    });
+
+    async function fetchSongsFromFirebase(){
+        try {
+            const querySnapshot=await getDocs(collection(db,"songs"));
+            let list:Song[]=[]
+            querySnapshot.forEach((doc) => {
+                let data={
+                    id:doc.id,
+                    song_name:doc.data().song_name,
+                    youtube_link:doc.data().youtube_link,
+                }
+                list.push(data)
+            });
+            setSongs([...list])
+        } catch (error:any) {
+            console.log(error)
+            err_toast(error.message)
+        }
+    }
+
     function handleSort(value:string){
         console.log(value)
     }
 
     let checkedBoxArrayValues:string[]=[]
-    function checkedBoxHandler(e:any) {
+    async function checkedBoxHandler(e:any) {
         let header:any=document.getElementById("header")
         if(!e.target.checked){
             const index = checkedBoxArrayValues.indexOf(e.target.value);
@@ -90,10 +93,10 @@ export default function Songs() {
             checkedBoxArrayValues=[]
         })
 
-        document.getElementById("deleteCheckedItems")?.addEventListener("click",async()=>{
+        async function deleteEvent(){
             try{
-                checkedBoxArrayValues.map((checkedItem:string)=>{
-                  console.log(checkedItem)
+                checkedBoxArrayValues.map(async (checkedItem:string)=>{
+                    await deleteDoc(doc(db,"songs",checkedItem))
                 })
                 header.innerHTML=`<p class="text-lg font-semibold">Team Ruiru Portal</p>`
                 for (let i = 0; i < checkedBoxArrayValues.length; i++) {
@@ -103,16 +106,37 @@ export default function Songs() {
                 }
             }catch(error:any){
                 console.log(error.message)
+                err_toast(error.message)
             }
-        })
+        }
+        document.getElementById("deleteCheckedItems")?.addEventListener("click",deleteEvent)
     }
+
+    async function handleCreateSong(e:any) {
+        try {
+            e.preventDefault()
+            let event:Song={
+                song_name:e.target.song_name.value,
+                youtube_link:e.target.youtube_link.value,
+            }
+            await addDoc(collection(db,"songs"),event);
+            e.target.reset()
+        } catch (error:any) {
+            console.log(error)
+            err_toast(error.message)
+        }
+    }
+
+    useEffect(()=>{
+        fetchSongsFromFirebase()
+    },[updateSong])
     return (
-        <div className="p-10">
-            <div className="mt-8 w-full rounded-lg border-[1px] text-sm">
+        <div className="p-10 flex gap-4 max-sm:flex-wrap">
+            <div className="mt-8 flex-grow rounded-lg border-[1px] text-sm">
                 <div className="flex flex-col py-6 px-8 border-b-[1px]">
                     <div className="flex justify-between items-center">
-                        <p className="text-[20px] text-[var(--gray-heading)] font-semibold">songs / Music</p>
-                        <button className="bg-[var(--theme-blue)] text-white flex rounded-md outline-none px-6 py-2 items-center justify-center">
+                        <p className="text-[20px] text-[var(--gray-heading)] font-semibold">Songs / Music</p>
+                        <button onClick={()=>setShowAddSongForm(true)} className="bg-[var(--theme-blue)] text-white flex rounded-md outline-none px-6 py-2 items-center justify-center">
                             <FaPlus className="w-4 h-4 mr-1"/>
                             <span>Add a new song</span>
                         </button>
@@ -146,22 +170,24 @@ export default function Songs() {
                             <th className="text-left">
                                 <input type="checkbox" disabled className="w-5 border-gray-400 focus:bg-[var(--theme-blue)] accent-[var(--theme-blue)] cursor-pointer h-5"/>
                             </th> 
-                            <th className="text-left">Song Name</th>
+                            <th className="text-left">Song</th>
                             <th className="text-left">Youtube Link</th>
                             <th className="text-left">Actions</th>
                         </tr>
                     </thead>
                     <tbody className='text-sm'>
-                        {events.map((schedule,index)=>{
+                        {songs.map((song)=>{
                             return(
-                                <tr title={`#${schedule.programme}`} key={index} className="text-[#64748B]">
+                                <tr title={`#${song.song_name}`} key={song.id} className="text-[#64748B]">
                                     <td className="text-left">
                                     <div>
-                                        <input id={`checkbox_${schedule.programme}`} type="checkbox" value={schedule.programme} onChange={checkedBoxHandler} className="checkbox w-5 border-gray-400 focus:bg-[var(--theme-blue)] accent-[var(--theme-blue)] cursor-pointer h-5"/>
+                                        <input id={`checkbox_${song.id}`} type="checkbox" value={song.id} onChange={checkedBoxHandler} className="checkbox w-5 border-gray-400 focus:bg-[var(--theme-blue)] accent-[var(--theme-blue)] cursor-pointer h-5"/>
                                     </div>
                                     </td>
-                                    <td className="text-left">{schedule.date}</td>
-                                    <td className="text-left">{schedule.programme}</td>
+                                    <td className="text-left">{song.song_name}</td>
+                                    <td className="text-left text-[var(--theme-blue)] underline">
+                                        <a href={song.youtube_link} target="_blank" rel="noreferrer noopener">{song.youtube_link}</a>
+                                    </td>
                                     <td className="text-left" title="Actions">
                                         <div className="rounded-[100px] py-3 px-2 w-fit hover:shadow-md hover:bg-gray-100 cursor-pointer">
                                             <MdMoreVert className="w-6 h-4"/>
@@ -173,6 +199,27 @@ export default function Songs() {
                     </tbody>
                 </table>
             </div>
+            {showAddSongForm?(
+                <div className="mt-8 h-fit rounded-lg border-[1px] text-sm p-4 w-[25vw]">
+                    <div className="flex justify-between items-center">
+                        <p className="text-[20px] font-semibold">Add a new song</p>
+                        <button title="close" onClick={()=>setShowAddSongForm(false)}>
+                            <MdClose className="w-5 h-5"/>
+                        </button>
+                    </div>
+                    <form onSubmit={handleCreateSong} className="flex mt-5 flex-col text-sm">
+                        <label className="mb-[8px] text-[#0f172a]" htmlFor="song_name">Song <span className="text-red-500">*</span></label>
+                        <div className="pb-4">
+                            <input id="song_name" name="song_name" type="text" className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`} placeholder="Name of the song" required/>
+                        </div>
+                        <label className="mb-[8px] text-[#0f172a]" htmlFor="youtube_link">Youtube Link <span className="text-red-500">*</span></label>
+                        <div className="pb-4">
+                            <input id="youtube_link" name="youtube_link" type="text" placeholder="https://www.youtube.com/watch?..." className={`px-[10px] w-full py-2 focus:outline-[var(--theme-blue)] focus:outline-[1px] bg-white border-[1px] rounded-lg`}/>
+                        </div>
+                        <button className="mt-5 capitalize py-3 px-6 text-white rounded-md bg-[var(--theme-blue)]">Submit</button>
+                    </form>
+                </div>
+            ):""}
         </div>
     );
 };
